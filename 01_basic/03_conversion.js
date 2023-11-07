371037d50ee1da1ca9886c48b2988f3bbe30a441
Uttam Darekar <133651968+darekar-uttam@users.noreply.github.com>
2023-11-07 04:55:19 +0000

    data types conversion

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -3,3 +3,13 @@
 let score = "34"
 let convertNum = Number(score)
 console.log(typeof(convertNum))
+
+console.log("1" + 2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
