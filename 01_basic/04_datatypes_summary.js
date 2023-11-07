a812a9ea1aeadc5c3c7a68e2c4c320dd9066913b
Uttam Darekar <133651968+darekar-uttam@users.noreply.github.com>
2023-11-07 05:22:37 +0000

    data types summary

---
+++ 01_basic/04_datatypes_summary.js
@@ -0,0 +1,22 @@
+//symbole declaration
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 734682938628n
+
+//array
+
+const heros = ["spidy", "hulk","capt"]
+
+//object
+let myObj = 
+{name:"uttam",
+age:22,
+}
+
+//fuction
+
+const myFucntion = function(){
+    console.log("demo")
+}
