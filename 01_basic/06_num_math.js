f4feb952e885a2027ee3779e0ee78eeb9dde56ae
Uttam Darekar <133651968+darekar-uttam@users.noreply.github.com>
2023-11-08 10:18:25 +0000

    number and maths

---
+++ 01_basic/06_num_math.js
@@ -0,0 +1,18 @@
+const score = 400
+ const balance = new Number(100)
+ console.log(balance);
+ 
+
+ const otherNum = 123.8999
+
+
+ console.log(otherNum.toPrecision(3));
+
+ const hundreds = 100000
+ console.log(hundreds.toLocaleString('en-IN'));
+
+
+ /////////--------------------------------------------------------MATHS
+
+ console.log(Math);
+
