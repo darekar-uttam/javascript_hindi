740a667331dd2a6dfb93422156efebf353b62e5a
Uttam Darekar <133651968+darekar-uttam@users.noreply.github.com>
2023-11-08 18:48:04 +0000

    date and time

---
+++ 01_basic/07_date.js
@@ -0,0 +1,21 @@
+// console.log("dates");
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toDateString());
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date ()
+
+console.log(newDate.getMonth());
