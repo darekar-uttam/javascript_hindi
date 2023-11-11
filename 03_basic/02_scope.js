46d77bc8d2d30279d14ad951eb69a9553cff8547
Uttam Darekar <133651968+darekar-uttam@users.noreply.github.com>
2023-11-11 08:37:29 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,41 @@
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "uttam"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+one()
+
+
+if (true) {
+    const username = "uttam"
+    if (username === "uttam") {
+        const website = "github"
+        // console.log(username)
+                
+    }
+    // console.log(website);    
+}
+
+// console.log(username);
+
+
+//------------intersting-----------------
+ function addOne(num){
+    return num + 1
+ }
