ed6fd903131f97a3028a304ad2920ed8f6e7b2aa
Uttam Darekar <133651968+darekar-uttam@users.noreply.github.com>
2023-11-12 07:48:16 +0000

    loop

--- 04_iteration/01.js
+++ 04_iteration/01.js
@@ -8,10 +8,30 @@
 //    console.log(element);
 // }
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`outer loopL ${i}`);
-    for (let j = 0; j <= 10; j++) {
-        //console.log(`inner loopL ${j}, outer loop ${i}`);
-        console.log(i + '*' + j + '=' + i*j);
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loopL ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         //console.log(`inner loopL ${j}, outer loop ${i}`);
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+// }
+
+
+//break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`detected 5`);
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`detected 5`);
+        continue
     }
+    console.log(`value of i is ${index}`);
+    
 }
