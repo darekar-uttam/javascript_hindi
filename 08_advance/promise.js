eb86b8d10c61bd90f4e5fa30ca4a2ee861be5076
Uttam Darekar <133651968+darekar-uttam@users.noreply.github.com>
2023-12-01 08:13:02 +0000

    promise second

--- 08_advance/promise.js
+++ 08_advance/promise.js
@@ -26,3 +26,94 @@ new Promise(function(resolve, reject){
 }).then(function(){
     console.log("Async two resolved");
 })
+
+
+//third promise
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Uttam", email: "uttam@gmail.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+
+//fourth promise
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "admin", password: "123"})
+        }
+        else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+promiseFour
+.then((user) => {
+    console.log();
+    return user.username
+})
+.then((username) => {
+    console.log(username);
+})
+.catch((error)=>{
+    console.log(error);
+})
+.finally(()=> console.log("Promise is resolve and rejected"))
+
+
+//promise five
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "javascript", password: "123"})
+        }
+        else{
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+})
+
+async function consumedPromiseFive(){
+    try {
+        const response  = await promiseFive
+    console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+consumedPromiseFive()
+
+
+const rquestUrl  = "https://api.github.com/users/darekar-uttam"
+
+// async function getUserDetail(){
+//     try {        
+//     const response = await fetch("https://api.github.com/users/darekar-uttam")
+//     const data = await response.json()
+//     console.log(data);
+//     } catch (error) {
+//         console.log(error);        
+//     }
+// }
+// getUserDetail()
+
+fetch('https://api.github.com/users/darekar-uttam')
+.then((response)=>{
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
